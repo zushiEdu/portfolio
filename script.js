62b382acd716898dc3c61b4332c609b3062d86d9
Ethan <hubee8329@wrdsb.ca>
2024-12-25 17:11:19 -0500

    Photography, awards, and projects navigation fixed.

--- script.js
+++ script.js
@@ -1,11 +1,36 @@
-var projectCounter = 0;
-var rowCounter = 0;
-var projectPageIndex = 1;
+let projectCounter = 0;
+let rowCounter = 0;
+let projectPageIndex = 1;
 const pageLength = 6;
-var masterProjects;
+let masterProjects;
+let pageAmount = 0;
+let photosContainer1Height = 0;
+let photosContainer2Height = 0;
 
-var contentContainer = document.getElementsByClassName('content')[0];
-var projectsContainer = document.getElementById('projects');
+let contentContainer = document.getElementsByClassName('content')[0];
+let projectsContainer = document.getElementById('projects');
+let photosContainer1 = document.getElementById('photoCol1');
+let photosContainer2 = document.getElementById('photoCol2');
+
+const urlParams = new URLSearchParams(window.location.search);
+const pageParam = urlParams.get('page');
+projectPageIndex = pageParam;
+
+fetch('/Data/photos.json')
+    .then(response => response.json())
+    .then(images => {
+        images.forEach((image) => {
+            let img = document.createElement('img');
+            img.src = image.url;
+            if (photosContainer1Height >= photosContainer2Height) {
+                photosContainer2Height += img.height;
+                photosContainer2.appendChild(img);
+            } else {
+                photosContainer1Height += img.height;
+                photosContainer1.appendChild(img);
+            }
+        })
+    });
 
 fetch('/Data/projects.json')
     .then(response => response.json())
@@ -15,20 +40,20 @@ fetch('/Data/projects.json')
         projects.forEach((project) => {
             if (project.display) {
                 projectCounter++;
-                var cell = document.createElement('td');
-                var section = document.createElement('section');
-                cell.id = project.title.replaceAll(" ", "_");
+                let cell = document.createElement('td');
+                let section = document.createElement('section');
+                cell.id = project.title.replaceAll(" ", "_") + "_cell";
 
                 if (project.thumb != null) {
-                    var image = document.createElement('img');
+                    let image = document.createElement('img');
                     image.setAttribute("class", "contentThumb");
                     image.src = project.thumb;
                     section.appendChild(image);
                 }
 
-                var innerList = document.createElement('ul');
+                let innerList = document.createElement('ul');
 
-                var title;
+                let title;
                 if (project.title != null) {
                     title = document.createElement('h3');
                     title.textContent = project.title;
@@ -36,37 +61,38 @@ fetch('/Data/projects.json')
 
                 }
 
-                var tldr;
+                let tldr;
                 if (project.tldr != null) {
                     tldr = document.createElement('p');
                     tldr.setAttribute("class", "tldr");
                     tldr.textContent = project.tldr;
                 }
 
-                var tileLi = document.createElement('li');
+                let tileLi = document.createElement('li');
                 tileLi.appendChild(title);
                 tileLi.appendChild(tldr);
                 innerList.appendChild(tileLi);
 
                 if (project.paragraph != null) {
-                    var more = document.createElement('a');
-                    var tileTag = project.title.replaceAll(" ", "_");
+                    let more = document.createElement('a');
+                    let tileTag = project.title.replaceAll(" ", "_");
                     more.href = "#" + tileTag;
                     more.setAttribute("class", "underlined");
                     more.textContent = "More";
-                    var morePage = document.createElement('div');
+                    more.id = project.title.replaceAll(" ", "_") + "_link";
+                    let morePage = document.createElement('div');
 
                     more.addEventListener("click", function (event) {
                         morePage.setAttribute("style", "display:block");
                     })
 
-                    var li = document.createElement('li');
+                    let li = document.createElement('li');
                     li.appendChild(more);
                     innerList.appendChild(li);
 
                     morePage.setAttribute("class", "page");
-                    var description = document.createElement('p');
-                    var back = document.createElement('a');
+                    let description = document.createElement('p');
+                    let back = document.createElement('a');
                     back.href = "#projects";
                     back.setAttribute("class", "underlined");
                     back.textContent = "Back";
@@ -77,16 +103,16 @@ fetch('/Data/projects.json')
 
                     morePage.appendChild(back);
 
-                    var title = document.createElement('h2');
+                    let title = document.createElement('h2');
                     title.innerText = project.title;
                     morePage.appendChild(title);
 
-                    var header = document.createElement('img');
+                    let header = document.createElement('img');
                     header.src = project.header;
                     header.setAttribute("class", "moreInfoHeader");
                     morePage.appendChild(header);
 
-                    var date = document.createElement('p');
+                    let date = document.createElement('p');
                     date.innerText = project.date;
                     date.setAttribute("style", "display:inline");
                     morePage.appendChild(date);
@@ -94,7 +120,7 @@ fetch('/Data/projects.json')
 
                     if (project.links != null) {
                         const urls = project.links;
-                        for (var j = 0; j < urls.length; j++) {
+                        for (let j = 0; j < urls.length; j++) {
                             const seperator = document.createElement('a')
                             seperator.textContent = " | ";
                             const link = document.createElement('a');
@@ -109,7 +135,7 @@ fetch('/Data/projects.json')
 
                     description.innerText = project.paragraph;
                     morePage.appendChild(description);
-                    morePage.setAttribute("id", tileTag);
+                    morePage.setAttribute("id", tileTag + "_page");
                     contentContainer.appendChild(morePage);
                 }
 
@@ -117,13 +143,13 @@ fetch('/Data/projects.json')
 
                 cell.appendChild(section);
 
-                var row = Math.round(projectCounter / 2);
+                let row = Math.round(projectCounter / 2);
                 if (document.getElementById(row)) {
                     tr = document.getElementById(row);
                     projectsContainer.appendChild(tr);
                     tr.appendChild(cell);
                 } else {
-                    var newRow = document.createElement('tr');
+                    let newRow = document.createElement('tr');
                     newRow.id = Math.round(projectCounter / 2);
                     newRow.appendChild(cell);
                     rowCounter++;
@@ -132,15 +158,19 @@ fetch('/Data/projects.json')
             }
         });
 
-        var projectsNav = document.createElement('ul');
+        let projectsNav = document.createElement('ul');
         projectsNav.id = 'projectsNav';
 
-        for (let k = 0; k < Math.ceil(projects.length) / pageLength; k++) {
-            var listItem = document.createElement('li');
-            var link = document.createElement('a');
+        pageAmount = Math.ceil(projects.length) / pageLength;
+
+        for (let k = 0; k < pageAmount; k++) {
+            let listItem = document.createElement('li');
+            let link = document.createElement('a');
             link.innerText = k + 1;
             link.setAttribute('onclick', 'loadPage(' + (k + 1) + ')');
-            link.setAttribute('href', '#projects');
+            // link.setAttribute('href', '#projects');
+            link.setAttribute('class', 'projectPagesNavText');
+            link.setAttribute('id', 'page' + (k + 1) + 'Text');
             listItem.setAttribute('id', 'page' + (k + 1) + 'Link');
             listItem.appendChild(link);
             projectsNav.appendChild(listItem);
@@ -149,24 +179,40 @@ fetch('/Data/projects.json')
         projectsContainer.appendChild(projectsNav);
 
         loadPage(projectPageIndex);
-        // document.getElementById(window.location.hash.replace('#', '')).setAttribute("style", "display:block");
+
+        if ((window.location.hash == '')) {
+            console.log("Landed on no content page, redirecting to projects.");
+            document.getElementById('projectsButton').click();
+        } else if (!(window.location.hash == '#projects' || window.location.hash == '#photography' || window.location.hash == '#awards' || window.location.hash == '#about-me')) {
+            console.log("Someone tried directing to a specific page, directing to page.");
+            document.getElementById('projectsButton').click();
+            document.getElementById(window.location.hash.replace("#", '') + "_link").click();
+        }
 
     });
 
 function loadPage(page) {
     projectPageIndex = page;
-    for (var i = 1; i <= rowCounter; i++) {
+
+    const url = new URL(window.location);
+    url.searchParams.set("page", projectPageIndex);
+    history.pushState(null, '', url);
+
+    for (let i = 1; i < pageAmount + 1; i++) {
+        if (i == projectPageIndex) {
+            document.getElementById('page' + i + 'Text').style.textDecoration = 'underline';
+        } else {
+            document.getElementById('page' + i + 'Text').style.textDecoration = 'none';
+        }
+    }
+
+    for (let i = 1; i <= rowCounter; i++) {
         document.getElementById(i).style.display = 'none';
     }
 
-    for (var i = (page * 3) - 2; i <= page * 3; i++) {
+    for (let i = (page * 3) - 2; i <= page * 3; i++) {
         if (document.getElementById(i) != undefined) {
             document.getElementById(i).style.display = 'table-row';
         }
     }
 }
-
-
-// if (document.getElementById(window.location.hash).style.display == '') {
-
-// }
